9073c02ea0d290ca5b86d7ad1ad458de42723808
karolh2000 <karolh@jfrog.com>
2022-08-15 09:38:31 -0700

    Workflow experiments init commit (arch

--- .github/workflows/sccache-linux.sh
+++ .github/workflows/sccache-linux.sh
@@ -3,5 +3,5 @@
 set -e
 # Download and install sccache in the Linux specific cargo directories
 mkdir -p /home/runner/.cargo/bin
-curl -o- -sSLf https://github.com/mozilla/sccache/releases/download/v0.2.15/sccache-v0.2.15-x86_64-unknown-linux-musl.tar.gz | tar --strip-components=1 -C /home/runner/.cargo/bin -xzf -
+curl -o- -sSLf https://github.com/mozilla/sccache/releases/download/v0.3.0/sccache-v0.3.0-aarch64-apple-darwin.tar.gz | tar --strip-components=1 -C /home/runner/.cargo/bin -xzf -
 chmod 755 /home/runner/.cargo/bin/sccache
