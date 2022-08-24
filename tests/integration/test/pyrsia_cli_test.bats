1c240a5476b05298de79a5d7efe9c063339428aa
karolh2000 <karolh@jfrog.com>
2022-08-24 15:11:54 -0700

    Refactoring, new test added.

--- tests/integration/test/pyrsia_cli_test.bats
+++ tests/integration/test/pyrsia_cli_test.bats
@@ -1,33 +1,44 @@
 #!/usr/bin/expect -f
 
+#tests timeout
+BATS_TEST_TIMEOUT=10
+
 setup() {
     load 'test_helper/common-setup'
     _common_setup
 }
 
-#@test "Pyrsia CLI HELP" {
-#  # run pyrsia ping
-#  run $PYRSIA_CLI_FOLDER/pyrsia help
-#  # check if pyrsia ping returns errors
-#  refute_output --partial 'Error'
-#}
-#
-#@test "Pyrsia CLI PING" {
-#  # run pyrsia ping
-#  run $PYRSIA_CLI_FOLDER/pyrsia ping
-#  # check if pyrsia ping returns errors
-#  refute_output --partial 'Error'
-#}
+@test "Testing 'pyrsia HELP' CLI, check if the help is shown..." {
+  # run pyrsia help
+  run $PYRSIA_CLI_FOLDER/pyrsia help
+  # check if pyrsia ping returns errors
+  refute_output --partial 'Error'
+}
 
-#@test "Pyrsia CLI LIST" {
-#  # run pyrsia ping
-#  run $PYRSIA_CLI_FOLDER/pyrsia list
-#  # check if pyrsia ping returns errors
-#  refute_output --partial 'Error'
-#}
+@test "Testing 'pyrsia PING' CLI, check if the node is up..." {
+  # run pyrsia ping
+  run $PYRSIA_CLI_FOLDER/pyrsia ping
+  # check if pyrsia ping returns errors
+  refute_output --partial 'Error'
+}
 
-@test 'Pyrsia CLI config edit' {
-    run $PYRSIA_CLI_FOLDER/pyrsia config --edit
+@test "Testing 'pyrsia STATUS' CLI, check if the node is up..." {
+  # run pyrsia ping
+  run $PYRSIA_CLI_FOLDER/pyrsia status
+  # check if pyrsia ping returns errors
+  refute_output --partial '0'
+}
+
+@test "Testing 'pyrsia LIST' CLI, check if the node is up..." {
+  # run pyrsia ping
+  run $PYRSIA_CLI_FOLDER/pyrsia list
+  # check if pyrsia ping returns errors
+  refute_output --partial '[]'
+}
+
+@test 'Pyrsia CLI CONFIG EDIT' {
+    run $PYRSIA_CLI_FOLDER/pyrsia config --show
+    refute_output --partial 'localhost'
 }
 
 #teardown() {
--- tests/integration/test/test_helper/common-setup.bash
+++ tests/integration/test/test_helper/common-setup.bash
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-PYRSIA_FOLDER=/tmp/pyrsia
-BATS_TEST_TIMEOUT=1000*1200 # the tests will timeout in 20 min
-
+PYRSIA_SRC_DIR=/tmp/pyrsia/ # TODO Change this to /tmp/source/pyrsia
 
 _common_setup() {
     load 'test_helper/bats-support/load'
@@ -10,15 +8,25 @@ _common_setup() {
     # get the containing directory of this file
     # use $BATS_TEST_FILENAME instead of ${BASH_SOURCE[0]} or $0,
     # as those will point to the bats executable's location or the preprocessed file respectively
-    PROJECT_ROOT="$( cd "$( dirname "$BATS_TEST_FILENAME" )/.." >/dev/null 2>&1 && pwd )"
+    # PROJECT_ROOT="$( cd "$( dirname "$BATS_TEST_FILENAME" )/.." >/dev/null 2>&1 && pwd )"
     # make executables in src/ visible to PATH
-    PATH="$PROJECT_ROOT/src:$PATH"
-    # git clone --branch karolh2000/integ_tests_bats https://github.com/karolh2000/pyrsia.git  /tmp/pyrsia
-    echo "Building the Pyrsia sources, it might take several minutes..." >&3
-    cargo build --profile=release --package=pyrsia_cli --manifest-path=$PYRSIA_FOLDER/Cargo.toml
-    echo "Building Pyrsia completed!" >&3
-    PYRSIA_CLI_FOLDER=$PYRSIA_FOLDER/target/release
-    echo "Building the Pyrsia Node docker image and starting the container, it might take several minutes..." >&3
-    docker-compose -f $PYRSIA_FOLDER/docker-compose.yml up -d
-    echo "Pyrsia Node docker container is up!" >&3
+    # PATH="$PROJECT_ROOT/src:$PATH"
+    local git_branch="karolh2000/integ_tests_bats"
+    # clone or update the sources
+    if [ -d $PYRSIA_SRC_DIR/.git ]; then
+       git --git-dir=$PYRSIA_SRC_DIR/.git fetch
+       git --git-dir=$PYRSIA_SRC_DIR/.git --work-tree=$PYRSIA_SRC_DIR merge origin/main
+    else
+      mkdir -p $PYRSIA_SRC_DIR
+      git clone --branch $git_branch https://github.com/karolh2000/pyrsia.git  $PYRSIA_SRC_DIR
+    fi;
+
+    printf "\t Building the Pyrsia sources, it might take several minutes... \n" >&3
+    cargo build --profile=release --package=pyrsia_cli --manifest-path=$PYRSIA_SRC_DIR/Cargo.toml
+    printf "\t Building Pyrsia completed! \n" >&3
+    PYRSIA_CLI_FOLDER=$PYRSIA_SRC_DIR/target/release
+    printf "\t Building the Pyrsia Node docker image and starting the container, it might take several minutes... \n" >&3
+    docker-compose -f $PYRSIA_SRC_DIR/docker-compose.yml up -d
+    printf "\t Pyrsia Node docker container is up! \n" >&3
+    printf "\t Starting the test... \n" >&3
 }
